beacf0728bcba979b34449d4f895a86573a6f7ae
sandeep-196 <pujarisubramanya73@gmail.com>
2025-12-03 11:57:40 +0530

    Update palindrome01.py

--- palindrome01.py
+++ palindrome01.py
@@ -1,10 +1,7 @@
 def is_palindrome(text):
-    text = text.lower()              # convert to lowercase
-    return text == text[::-1]        # compare with reversed string
-
-# Example usage
-s = input("Enter a string: ")
-if is_palindrome(s):
-    print("Palindrome")
+    return text == text[::-1]
+word="MALAYALAM"
+if is_palindrome(word):
+     print("this string is palindrome")
 else:
-    print("Not a palindrome")
+     print("This is not palindrome")
